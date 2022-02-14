e23ba52e4018aaa825186c376e0a8fe76a7c3437
PavelStar <42155287+PavelStar@users.noreply.github.com>
2022-02-14 14:26:32 +0300

    refactoring

--- src/API/ApiService.ts
+++ src/API/ApiService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 class ApiService {
 	async getData() {
-		const url: string = "https://mocki.io/v1/4ad914c0-6fdc-4d3f-9adb-f0757f304860";
+		const url: string = "https://mocki.io/v1/3136a775-993a-4d14-a09c-5aad4cae377c";
 		const res = await axios.get(url);
 		return res.data;
 	}
